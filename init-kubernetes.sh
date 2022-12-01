d963995d0939bced560c37798ec57b757016683e
henricker <henriquevieira@alu.ufc.br>
2022-12-01 09:37:12 -0300

    feat: add health, liveness, readnesss and startup probes

--- init-kubernetes.sh
+++ init-kubernetes.sh
@@ -1,4 +1,4 @@
-#!/usr/env/bin bash
+#!/usr/bin/env bash
 
 # This script is used to initialize the kubernetes cluster
 
@@ -9,9 +9,9 @@ kind create cluster --config ./k8s/kind.yml --name=fullcycle
 kubectl cluster-info --context kind-fullcycle
 
 # RUN ENVS
-kubectl apply -f ./k8s/envs/config-map-family.yml
-kubectl apply -f ./k8s/envs/config-map.yml
-kubectl apply -f ./k8s/envs/secret.yml
+kubectl apply -f ./k8s/env/config-map-family.yml
+kubectl apply -f ./k8s/env/config-map.yml
+kubectl apply -f ./k8s/env/secret.yml
 
 # RUN SERVICES
 kubectl apply -f ./k8s/services/loadBalance-service.yml
@@ -20,4 +20,4 @@ kubectl apply -f ./k8s/services/loadBalance-service.yml
 kubectl apply -f ./k8s/deployments/deployment.yml
 
 # port-forward
-kubectl port-forward service/goserver 8000:80
+#kubectl port-forward service/goserver 8000:80
--- server.go
+++ server.go
@@ -6,7 +6,10 @@ import (
 	"fmt"
 	"log"
 	"io/ioutil"
+	"time"
 )
+
+var startedAt = time.Now()
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
@@ -29,9 +32,22 @@ func Family(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(data))
 }
 
+func Healthz(w http.ResponseWriter, r *http.Request) {
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() < 10 || duration.Seconds() > 30 {
+		w.WriteHeader(500)
+		w.Write([]byte("Not healthy"))
+	}else {
+		w.WriteHeader(200)
+		w.Write([]byte("Healthy"))
+	}
+}
+
 func main() {
 	http.HandleFunc("/", Hello)
 	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/family", Family)
+	http.HandleFunc("/healthz", Healthz)
 	http.ListenAndServe(":80", nil)
 }
